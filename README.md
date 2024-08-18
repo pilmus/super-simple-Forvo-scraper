ce5474e92c7dc60a930cb2b8ce5e5df91d9be91b
Maaike Visser <mebpvisser@gmail.com>
2024-08-18 09:33:33 +0200

    Adapt script to work with command line arguments

--- README.md
+++ README.md
@@ -9,16 +9,21 @@ I'll quote from their [README](https://github.com/NSBum/forvo_scraper_example/bl
 ## Why this scraper?
 In an effort to understand the forvo scraper example code, I decided to start from the ground up with only the most basic elements I need for my own usecase.
  
-What is does is:
+The script follows these steps:
 - Open API link in the browser
 - Find & click first link to a sound file
 - Rename all downloaded sound files to the word they contain
 
 ## Usage
 
+This script is meant to be called from the command line.
+
+Steps to install and run:
 - Install `selenium` and `python-dotenv` (this is only necessary if you want to load your api key through a `.env` file. I only did this so I wouldn't put my own api key on GitHub.)
 - Put the words you want in `wordlist.txt`.
 - Copy `example.env` to `.env` and change the values.
-- `python selenium_scrape.py`.
+- `python selenium_scrape.py <word_or_file> -d <destination_folder> (-l <language_code>)`.
 
-If you want to download 
+## Requirements
+- Developed under Python 3.12.4. Earlier versions may be compatible but are not tested.
+- Developed on Windows, should be system agnostic but that's also not tested.
--- selenium_scrape.py
+++ selenium_scrape.py
@@ -1,72 +1,104 @@
+import argparse
 import os
+from pathlib import Path
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-api_key = os.getenv('API_KEY')
-wordlistfile = os.getenv('WORD_LIST')
-download_dir = os.getenv('DOWNLOAD_DIR')
-lang = os.getenv('LANG')
+api_key = os.getenv("API_KEY")
+base_url_str = f"https://apifree.forvo.com/action/word-pronunciations/format/json/word/QUERY/language/LANGUAGE_CODE/key/{api_key}/"
 
 
-base_url = f"https://apifree.forvo.com/action/word-pronunciations/format/json/word/QUERY/language/{lang}/key/{api_key}/"
-
-
-reverse_wordlist = []
-try:
-    driver = webdriver.Firefox()
-    with open (wordlistfile,'r',encoding='utf-8') as fp:
+def download_words_in_file(wordlistfile: Path, base_url: str, dest_dir: Path):
+    with open(wordlistfile, "r", encoding="utf-8") as fp:
         words = fp.read().splitlines()
+    download_list_of_words(words, base_url, dest_dir)
 
+
+def download_single_word(word: str, base_url: str, dest_dir: Path):
+    download_list_of_words([word], base_url, dest_dir)
+
+
+def download_list_of_words(words: list[str], base_url: str, dest_dir: Path):
+    reverse_wordlist = []
+    try:
+        driver = webdriver.Firefox()
         for word in words:
-            url = base_url.replace('QUERY', word)
-            
+            url = base_url.replace("QUERY", word)
+
             driver.get(url)
-            
+
             try:
-                sound_file = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#\/items\/0\/pathmp3 > .treeValueCell a")))
-                
+                sound_file = WebDriverWait(driver, 2).until(
+                    EC.element_to_be_clickable(
+                        (By.CSS_SELECTOR, "#\/items\/0\/pathmp3 > .treeValueCell a")
+                    )
+                )
+
                 sound_file.click()
                 reverse_wordlist.append(word)
 
-            except (NoSuchElementException, StaleElementReferenceException):
-                print(f'{word} not found.')
+            except Exception:
+                print(f"{word} not found.")
             sleep(1)
-except Exception as e:
-    print(e)
-finally:            
-    driver.quit()
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
 
-reverse_wordlist = reversed(reverse_wordlist)
+    reverse_wordlist = reversed(reverse_wordlist)
 
-downloaded_files = os.listdir(download_dir)
-downloaded_files.sort(key=lambda x: os.path.getmtime(os.path.join(download_dir,x)),reverse=True)
+    downloaded_files = os.listdir(dest_dir)
+    downloaded_files.sort(
+        key=lambda x: os.path.getmtime(os.path.join(dest_dir, x)), reverse=True
+    )
 
-for word,old_filename in zip(reverse_wordlist, downloaded_files):
-    new_filename = f"{word}.mp3"
-    new_filename_path = os.path.join(download_dir,new_filename)
-    old_filename_path = os.path.join(download_dir,old_filename)
-    
-    if os.path.exists(new_filename_path):
-        os.remove(new_filename_path)
-    os.rename(old_filename_path,new_filename_path)
+    for word, old_filename in zip(reverse_wordlist, downloaded_files):
+        new_filename = f"{word}.mp3"
+        new_filename_path = os.path.join(dest_dir, new_filename)
+        old_filename_path = os.path.join(dest_dir, old_filename)
 
-
-
-# 
+        if os.path.exists(new_filename_path):
+            os.remove(new_filename_path)
+        os.rename(old_filename_path, new_filename_path)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Forvo Word Downloader",
+        description="Downloads a (list of) word(s) through the Forvo API, using Selenium.",
+    )
+    parser.add_argument(
+        "-w",
+        "--word-or-file",
+        help="Either a single word or the path to a file with multiple words, one word per line.",
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--destination",
+        help="Path to destination folder of the sound files.",
+        required=True,
+    )
+    parser.add_argument(
+        "-l", "--language", help="Forvo language code. Default: ko.", default="ko"
+    )
 
+    args = parser.parse_args()
 
-# - go through list of words and download them with the selenium script
-# -- if a word isn't found, remove it from the list (or don't prepend it to the reversed list i guess)
-# - in reverse order, go through the words in the downloaded folder 
-# -- rename them to the correct word
+    base_url = base_url_str.replace("LANGUAGE_CODE", args.language)
+    destination_folder = Path(args.destination)
+    word_or_file = args.word_or_file
+    if os.path.isfile(word_or_file):
+        download_words_in_file(Path(word_or_file), base_url, destination_folder)
+    else:
+        download_single_word(word_or_file, base_url, destination_folder)
